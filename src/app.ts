d2389f2bbef08af37e09eae08771911da9dce30f
harsh-singh-repository <hk5425901@gamil.com>
2026-08-12 21:38:04 +0530

    Added Express and DockerFile

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (_req, res) => {
+  res.json({
+    success: true,
+    message: "Server Running Healthy 🚀",
+  });
+});
+
+app.get("/", (_req, res) => {
+  res.json({
+    success: true,
+    message: "Server Running 🚀",
+  });
+});
+
+export default app;
